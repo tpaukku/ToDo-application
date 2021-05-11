87857690c44acfb1a1a46b21110416a35f3f3f2d
Teemu Paukku <teemu.paukku@iki.fi>
2021-05-11 15:27:17 +0300

    Add Todo-app, create Todo-model
     On branch development
     Changes to be committed:
    	modified:   accounts/admin.py
    	modified:   config/settings.py
    	new file:   todo/__init__.py
    	new file:   todo/admin.py
    	new file:   todo/apps.py
    	new file:   todo/migrations/0001_initial.py
    	new file:   todo/migrations/__init__.py
    	new file:   todo/models.py
    	new file:   todo/tests.py
    	new file:   todo/views.py

---
+++ backend/todo/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Todo
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['author', 'text']
+
+admin.site.register(Todo, TodoAdmin)
+
+# Note to self, alternative way of registering to admin
+# @admin.register(Todo)
+# class TodoAdmin(admin.ModelAdmin):
+#     list_display = ['author', 'text']
---
+++ backend/todo/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+class Todo(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.CharField(max_length=150)
+    done_status = models.BooleanField(default=False)
+    order = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author}, {self.text}"
