d36df4e2be4481836953a9702b6e05b717374806
Teemu Paukku <teemu.paukku@iki.fi>
2021-05-17 13:27:24 +0300

    Add Swagger and Redoc for documenting API schema
    New endpoints at
      /api/v1/swagger
      /api/v1/swagger.json or.yaml
      /api/v1/redoc
    
     Changes to be committed:
    	modified:   Pipfile
    	modified:   Pipfile.lock
    	modified:   config/settings.py
    	modified:   config/urls.py
    	modified:   db.sqlite3

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,7 +14,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Todo API',
+        default_version='v1',
+        description='Todo API with multiple users and user authentication',
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="contact@snippets.local"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -24,4 +41,10 @@ urlpatterns = [
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
+    re_path(r'^api/v1/swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/v1/swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/v1/redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
